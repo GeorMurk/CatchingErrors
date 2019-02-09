1f6c1366461768b33fcaaa57d56c90088c51e8d7
George Kiragu <45111839+GeorMurk@users.noreply.github.com>
2019-02-10 02:52:30 +0300

    Add files via upload

---
+++ CatchingErrors.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+#!Catching Errors
+
+digit = float(input("Enter a Digit: "))
+print(digit)
+
+#! The above code is going to work just perfectly...
+
+#!Try Except blocks
+
+try:
+    number = int(input("Enter a Number: "))
+    print(number)
+except:
+    print("Invalid Input")
+
+#!the above code is going to work just fine but instead of python yelling to us, it going to give us a prompt inform of a comment
+
+
+#!Go and Test it Out!!!!
